6d4d19f0acceafc94488df00b82b7147564811f3
Kaan Ulupinar <kaan.ulupinar@gmail.com>
2018-10-11 11:24:29 -0700

    picture actually works now

--- App.js
+++ App.js
@@ -5,81 +5,79 @@ import {
   createStackNavigator,
 } from 'react-navigation';
 import { padStart } from 'lodash';
+import { PhaseScreen } from './phasescreen';
 
 const background = './components/background.jpg';
 
-function PhaseScreen(props) {
-  let date = props.navigation.state.params.day.dateString;
-  let moonphase = 0;
-  if (date.substring(5,7) === "02") {
-    moonphase = 31;
-  }
-  if (date.substring(5,7) === "03") {
-    moonphase = 59;
-  }
-  if (date.substring(5,7) === "04") {
-    moonphase = 90;
-  }
-  if (date.substring(5,7) === "05") {
-    moonphase = 120;
-  }
-  if (date.substring(5,7) === "06") {
-    moonphase = 151;
-  }
-  if (date.substring(5,7) === "07") {
-    moonphase = 181;
-  }
-  if (date.substring(5,7) === "08") {
-    moonphase = 212;
-  }
-  if (date.substring(5,7) === "09") {
-    moonphase = 243;
-  }
-  if (date.substring(5,7) === "10") {
-    moonphase = 273;
-  }
-  if (date.substring(5,7) === "11") {
-    moonphase = 304;
-  }
-  if (date.substring(5,7) === "12") {
-    moonphase = 334;
-  }
-  moonphase = (moonphase + parseInt(date.substring(8,10)))*24 - 23;
-  moonphase.toString();
-  moonphase = padStart(moonphase, 4, '0');
-  // moonnumber = ('./components/scripts/moon.' + moonphase + '.jpg').toString();
-  // alert(moonphase);
-  return (
-    <View>
-      <Image
-          style={{
-            alignSelf: 'center',
-            height: 400,
-            width: '100%',
-            borderWidth: 100,
-            borderRadius: 75
-          }}
-          source={{uri: `https://svs.gsfc.nasa.gov/vis/a000000/a004600/a004604/frames/730x730_1x1_30p/moon.${moonphase}.jpg`}}      
-          resizeMode="cover"
-      />
-      <Text>{props.navigation.state.params.day.dateString}</Text>
-    </View>
-  );
-}
+// function PhaseScreen(props) {
+//   let date = props.navigation.state.params.day.dateString;
+//   let moonphase = 0;
+//   if (date.substring(5,7) === "02") {
+//     moonphase = 31;
+//   }
+//   if (date.substring(5,7) === "03") {
+//     moonphase = 59;
+//   }
+//   if (date.substring(5,7) === "04") {
+//     moonphase = 90;
+//   }
+//   if (date.substring(5,7) === "05") {
+//     moonphase = 120;
+//   }
+//   if (date.substring(5,7) === "06") {
+//     moonphase = 151;
+//   }
+//   if (date.substring(5,7) === "07") {
+//     moonphase = 181;
+//   }
+//   if (date.substring(5,7) === "08") {
+//     moonphase = 212;
+//   }
+//   if (date.substring(5,7) === "09") {
+//     moonphase = 243;
+//   }
+//   if (date.substring(5,7) === "10") {
+//     moonphase = 273;
+//   }
+//   if (date.substring(5,7) === "11") {
+//     moonphase = 304;
+//   }
+//   if (date.substring(5,7) === "12") {
+//     moonphase = 334;
+//   }
+//   moonphase = (moonphase + parseInt(date.substring(8,10)))*24 - 23;
+//   moonphase.toString();
+//   moonphase = padStart(moonphase, 4, '0');
+//   // moonnumber = ('./components/scripts/moon.' + moonphase + '.jpg').toString();
+//   // alert(moonphase);
+//   return (
+//     <View>
+//       <Image
+//           style={{
+//             alignSelf: 'center',
+//             height: 400,
+//             width: '100%',
+//             borderWidth: 100,
+//             borderRadius: 75
+//           }}
+//           source={{uri: `https://svs.gsfc.nasa.gov/vis/a000000/a004600/a004604/frames/730x730_1x1_30p/moon.${moonphase}.jpg`}}      
+//           resizeMode="cover"
+//       />
+//       <Text>{props.navigation.state.params.day.dateString}</Text>
+//     </View>
+//   );
+// }
 class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
     // this.state = {
-    //   currentMonth
+    //   isLoading = true
     // };
   }
   static navigationOptions = { title: 'Phases      ' };
   render() {
     const resizeMode = 'cover';
     const { navigate } = this.props.navigation;
-    // const handleDayChange = () => {
-
-    // }
     return (
       <View style={styles.container}>
       <View
